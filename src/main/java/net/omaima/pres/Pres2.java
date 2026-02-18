0323c4af67813c00d2a9b59673847fcd4b9e1c95
Omaima <omaima.assif-etu@etu.univh2c.ma>
2026-02-18 22:13:17 +0000

    fixing things  ..

--- src/main/java/net/omaima/pres/Pres2.java
+++ src/main/java/net/omaima/pres/Pres2.java
@@ -6,10 +6,12 @@ import net.omaima.metier.IMetier;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class Pres2 {
-    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+    // FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(new File("config.txt"));
 
         String daoClassName = scanner.nextLine();
@@ -21,6 +23,15 @@ public class Pres2 {
         String metierClassName = scanner.nextLine();
         Class cMetier = Class.forName(metierClassName);
         IMetier metier = (IMetier) cDao.getConstructor(IDao.class).newInstance(dao);
+
+        //injection via le setter :)
+        //IMetier metier = (IMetier) cMetier.getConstructor().newInstance();
+        //Method setDao = cMetier.getDeclaredMethod("setDao", IDao.class); // chercher une methode setDao qui recois un objet de type cDao
+
+        //setDao.invoke(metier, dao);
+
+
+        System.out.println("RES ="+ metier.calcul());
 
     }
 }
